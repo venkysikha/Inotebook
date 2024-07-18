a7e443ccce1d195586295018e26fc3e96b9580fa
venkysikha <venkysikha3@gmail.com>
2024-07-18 19:27:50 +0530

    third commit

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,24 @@
 const connectToMongo = require('./db');
 const express = require('express');
+connectToMongo();
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 // Connect to MongoDB
-connectToMongo();
+app.get('/',(req,res)=>
+{
+  res.send('hello venky');
+}),
+
+// available routes 
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/note',require('./routes/note'));
+
+app.listen(port,()=>
+{
+  console.log(`example app listening at http://localhost:${port}`);
+})
 
 app.use(express.json());
-
-// Define your routes here
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
---
+++ backend/routes/auth.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const router=express.Router();
+const User=require('../models/User');
+
+// create user using the endpoint :post 
+router.get('/',(req,res)=>
+{
+    console.log(req.body);
+    const user=User(req.body);
+    user.save();
+    res.send(req.body);
+    
+})
+
+module.exports=router
